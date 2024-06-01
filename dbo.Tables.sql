f0536d8dea0202417e051504b64410decf37c497
Jesse Dalton <jmdalton0@gmail.com>
2024-05-31 18:03:59 -0600

    fetch?

---
+++ dbo.Tables.sql
@@ -0,0 +1,18 @@
+﻿CREATE TABLE [dbo].[Users] (
+    [id]        INT           IDENTITY (1, 1) NOT NULL,
+    [username]  NVARCHAR (64) NOT NULL,
+    [password]  NVARCHAR (64) NOT NULL,
+    [firstname] NVARCHAR (64) NOT NULL,
+    [lastname]  NVARCHAR (64) NOT NULL,
+    [sex]       BIT           NOT NULL,
+    [age]       INT           NOT NULL,
+    [state]     NVARCHAR (64) NOT NULL,
+    PRIMARY KEY CLUSTERED ([id] ASC)
+);
+
+CREATE TABLE [dbo].[Games] (
+    [id]     INT            IDENTITY (1, 1) NOT NULL,
+    [game]   NVARCHAR (512) NULL,
+    PRIMARY KEY CLUSTERED ([id] ASC)
+);
+
